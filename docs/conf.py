483e910d39b3f64d6d55a6e7b13e9df190d78c84
Tim Dorau <scharka01@googlemail.com>
2021-08-10 20:53:04 +0200

    Add Install and Support documentation file

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../spacetoolbox/isentropicflow/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,7 +33,8 @@ release = '0.01'
 import sphinx_rtd_theme
 
 extensions = [
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
