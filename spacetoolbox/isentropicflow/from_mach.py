5a8a382106bc5f2732a800df3da0e42411d435e6
Tim Dorau <scharka01@googlemail.com>
2021-08-18 08:55:36 +0200

    Rename insentropicflow

---
+++ spacetoolbox/isentropicflow/from_mach.py
@@ -0,0 +1,77 @@
+def pressure_to_pressure_total(mach, gamma):
+    r"""Calculates the pressure ratio given the mach number Ma.
+
+    .. math::
+        \frac{p}{p_{t}} = \left ( 1 + \frac{\gamma -1}{2} M^2 \right )^{\frac{-\gamma}{(\gamma - 1)}}
+
+    Parameters
+    ----------
+    mach : float
+        Mach number
+
+    gamma : float
+        Specific heat ratio
+
+    Returns
+    -------
+    pressure_to_pressure_total : float
+        Pressure ratio   
+
+    """
+    pressure_to_pressure_total = (1 + ((gamma - 1) / 2) * mach**2) ** (-gamma /
+                                 (gamma - 1))
+
+    return pressure_to_pressure_total
+
+
+def temperature_to_temperature_total(mach, gamma):
+    r"""Calculates the temperature ratio given the mach number Ma.
+
+    .. math::
+        \frac{T}{T_{t}} = \left ( 1 + \frac{\gamma -1}{2} M^2 \right )^{-1}
+
+    Parameters
+    ----------
+    mach : float
+        Mach number
+
+    gamma : float
+        Specific heat ratio
+
+    Returns
+    -------
+    temperature_to_temperature_total : float
+        Temperature ratio  
+
+    """
+    temperature_to_temperature_total = (1 + ((gamma - 1) / 2) * mach**2) ** (-1)
+
+    return temperature_to_temperature_total
+
+
+def rho_to_rho_total(mach, gamma):
+    r"""Calculates the density ratio given the mach number Ma.
+
+    .. math::
+        \frac{\rho}{\rho_{t}} = \left ( 1 + \frac{\gamma -1}{2} M^2 \right )
+            ^{\frac{-1}{(\gamma - 1)}}
+
+    Parameters
+    ----------
+    mach : float
+        Mach number
+
+    gamma : float
+        Specific heat ratio
+
+    Returns
+    -------
+    rho_to_rho_total : float
+        Density ratio  
+
+    """
+    rho_to_rho_total = (1 + ((gamma - 1) / 2) * mach**2) ** (-1 / (gamma - 1))
+
+    return rho_to_rho_total
+
+print(rho_to_rho_total(2, 1.4))
