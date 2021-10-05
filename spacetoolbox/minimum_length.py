d4eb4cdbd15aa7a2212f7d19652b4d8c42607bd2
Tim Dorau <scharka01@googlemail.com>
2021-10-05 16:53:25 +0200

    Add Prandtl Meyer function from mach number

--- spacetoolbox/minimum_length.py
+++ spacetoolbox/minimum_length.py
@@ -1,14 +1,14 @@
 import math
 import numpy as np
 import random
+from scipy.optimize import fsolve
 
 def prandtl_meyer_function_from_angle(gamma, prandtl_meyer_angle):
     r"""Calculates the Mach number given the Prandtl-Meyer angle
 
     Args:
-        prandtl_meyer_angle: 
-            (along nozzle axis)
-        gamma:  
+        prandtl_meyer_angle: Prandtl meyer angle in degree 
+        gamma: Heat capacity ratio 
             
     Returns:
         Mach number
@@ -17,9 +17,26 @@ def prandtl_meyer_function_from_angle(gamma, prandtl_meyer_angle):
     N_MACH = 100000
     for i in range(1, N_MACH  + 1):
         mach = random.uniform(0, 5)
+    
+
 
     return mach
 
-def prandtl_meyer_function_from_mach(mach, gamma):
+def prandtl_meyer_function_solver_form(mach, gamma):
+    r"""Calculates the Prandtl-Meyer angle given the mach number
+
+    Args:
+        mach: Mach number
+        gamma: Heat capacity ratio 
+            
+    Returns:
+        Prandtl-Meyer angle in radians
+    """
+    prandtl_meyer_angle = math.sqrt((gamma + 1) / (gamma - 1)) * \
+        math.atan(math.sqrt((gamma - 1) / (gamma + 1)
+        * (mach ** 2 - 1))) - math.atan(math.sqrt(mach ** 2
+        - 1))
+
+    return prandtl_meyer_angle
 
 prandtl_meyer_function_from_angle(36, 1.4)
