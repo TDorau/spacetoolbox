e4129a3bd8907440e53a638438f60db07b6bbf99
Carlos <carlostapiaman@gmail.com>
2021-10-23 19:55:57 +0200

    Adjusted precision and rounding

--- spacetoolbox/Area_Mach_relation/area_mach_relation.py
+++ spacetoolbox/Area_Mach_relation/area_mach_relation.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -16,10 +17,40 @@ def area_to_mach(radius_local):
         \left( \frac{A}{A^*} \right)^2=\frac{1}{M^2}\left[ \frac{2}{\gamma+1}\left( 1+\frac{\gamma-1}{2}M^2 \right)
         \right]^\frac{\gamma+1}{\gamma-1}
 
-        Output values can be verified using the Annex in [1].
+        Output values can be verified using the Appendix A in [1].
 
         Returns
         a mach number value corresponding to the given local area's radius.
     """
-gamma = 1.4
-radius_throat = 4.3263
+    gamma = 1.4
+    radius_throat = 4.3263
+    tolerance = 0.000001
+    mach_no = 0.00001
+    step_size = 0.000001
+
+    # local area ratio is the local area divided by the throat area
+    local_area_ratio = (radius_local ** 2) / (radius_throat ** 2)
+
+    # left side "ls" of the area-mach relation equation
+    ls = local_area_ratio**2
+
+    # right side "rs" of the area-mach relation equation
+    rs = (1/mach_no**2)*(2*(1+((gamma-1)*mach_no**2)/2)/(gamma+1))**((gamma+1)/(gamma-1))
+
+    # following, a while loop that compares the right side and the left side,
+    # when the ratio rs/ls != 1 (within a specified tolerance, e.g. 1%), the mach number mach_no is changed
+    # this is iterated until the corresponding mach number is found
+
+    i = rs / ls
+
+    while i < (1 - tolerance) or i > (1 + tolerance):
+        mach_no = mach_no + step_size
+        rs = (1 / mach_no ** 2) * (2 * (1 + ((gamma - 1) * mach_no ** 2) / 2) / (gamma + 1)) ** (
+                    (gamma + 1) / (gamma - 1))
+        i = rs / ls
+    else:
+        mach_no = np.around(mach_no, decimals=3)
+        print(mach_no)
+    return mach_no
+
+area_to_mach(4.3263)
